9c47d995973fb5eaf03f0f49469c03b395993be0
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-12 08:52:51 +0100

    🔒️ Routers configuration

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,23 @@ import Home from '../views/Home.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import('../components/CreateComponent')
+  },
+  {
+    path: '/view',
+    name: 'view',
+    component: () => import('../components/ListComponent')
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit',
+    component: () => import('../components/EditComponent')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
