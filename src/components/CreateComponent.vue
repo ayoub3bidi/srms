121d1c9655384537e824dd4bd481c17844098001
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-12 09:21:54 +0100

    ✅ Creating creation form

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <!-- Content goes here -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center display-6">Create Student</h3>
+                </div>
+                <div class="card-body">
+                    <form @submit.prevent="handleSubmitForm">
+                        <div style="padding-bottom: 10px;" class="form-group">
+                            <input type="text" class="form-control" placeholder="Student name" v-model="student.name" required>
+                        </div>
+
+                        <div style="padding-bottom: 10px;" class="form-group">
+                            <input type="email" class="form-control" placeholder="Student email" v-model="student.email" required>
+                        </div>
+
+                        <div style="padding-bottom: 10px;" class="form-group">
+                            <input type="text" class="form-control" placeholder="Student phone number" v-model="student.phone" required>
+                        </div>
+
+                        <div class="form-group text-center">
+                            <button class="btn btn-outline-success">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -10,7 +33,15 @@
     export default {
         data() {
             return {
+                student: {
+                    name: '',
+                    email: '',
+                    phone: ''
+                }
             }
+        },
+        methods: {
+            handleSubmitForm() {}
         }
     }
 </script>
